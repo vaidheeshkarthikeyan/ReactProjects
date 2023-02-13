79b7f2bbfd4fbf179dc6357ce5d84e6904477aa5
vaidheeshkarthikeyan <vaidheesh.karthikeyan@gmail.com>
2023-02-13 22:27:29 +0530

    My first React application

---
+++ pda/src/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "bulma/css/bulma.css";
+import ProfileCard from "./ProfileCard";
+
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
+function App() {
+  return (
+    <div>
+      <section className="hero is-info">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description="Alexa was made by Amazon"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana"
+                image={CortanaImage}
+                description="Cortana was made by Microsoft"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={SiriImage}
+                description="Siri was made by Apple"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default App;
